c0b6a641636180c1acd63686df5b4671bf1718df
M <mt.dev.2023@gmail.com>
2024-12-31 23:06:55 +0100

    js script added

--- script.js
+++ script.js
@@ -1,81 +1,76 @@
-const title = document.querySelector("h1")
-const letters = [...document.querySelectorAll('h1 span')
-]
+const titleH1 = document.querySelector("h1");
+const lettersH1 = [...document.querySelectorAll("h1 span")];
 
-title.addEventListener("mouseenter", handleLetters);
-title.addEventListener("mouseleave", handleLetters);
+const titleH2 = document.querySelector("h2");
+const lettersH2 = [...document.querySelectorAll("h2 span")];
 
+titleH1.addEventListener("mouseenter", () => handleLetters(lettersH1, "h1 span"));
+titleH1.addEventListener("mouseleave", () => handleLetters(lettersH1, "h1 span"));
+
+titleH2.addEventListener("mouseenter", () => handleLetters(lettersH2, "h2 span"));
+titleH2.addEventListener("mouseleave", () => handleLetters(lettersH2, "h2 span"));
 
 let isAnimatingIn = false;
 let calledOut = false;
 let animOpened = false;
 
-
-function handleLetters() {
-    if(animOpened){
-        animOut();
+function handleLetters(letters, target) {
+    if (animOpened) {
+        animOut(target);
     }
-    if(isAnimatingIn) {
-       calledOut = true;
-       return;
+    if (isAnimatingIn) {
+        calledOut = true;
+        return;
     }
 
     isAnimatingIn = true;
 
     const animPromise = new Promise((resolve) => {
-    animIn()
-    setTimeout(() => {
-        resolve()
-    }, 750)
-})
+        animIn(target);
+        setTimeout(() => {
+            resolve();
+        }, 750);
+    });
 
-animPromise.then(() => {
-    isAnimatingIn = false;
-    if(calledOut) {
-    animOut()
-    calledOut = false;
-   } else if (!calledOut){
-    animOpened = true;
-   }
-})
-
+    animPromise.then(() => {
+        isAnimatingIn = false;
+        if (calledOut) {
+            animOut(target);
+            calledOut = false;
+        } else if (!calledOut) {
+            animOpened = true;
+        }
+    });
 }
 
-
-
-
-
-
-
-function animIn() {
+function animIn(target) {
     anime({
-        targets: "h1 span",
-        translateX: function(){
-            return anime.random(-250,250)
+        targets: target,
+        translateX: function () {
+            return anime.random(-250, 250);
         },
-        translateY: function(){
-            return anime.random(-250,250)
+        translateY: function () {
+            return anime.random(-250, 250);
         },
-        translateZ: function(){
-            return anime.random(-2000,750)
+        translateZ: function () {
+            return anime.random(-2000, 750);
         },
-        rotate: function(){
-            return anime.random(-250,250)
+        rotate: function () {
+            return anime.random(-250, 250);
         },
         easing: "easeOutCirc",
-        duration: 750
-    }) 
+        duration: 750,
+    });
 }
 
-
-function animOut() {
+function animOut(target) {
     anime({
-        targets: "h1 span",
+        targets: target,
         translateX: 0,
         translateY: 0,
         translateZ: 0,
         rotate: 0,
         easing: "easeInQuad",
-        duration: 750
-    }) 
+        duration: 750,
+    });
 }
